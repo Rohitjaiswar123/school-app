84ee7233e2be142d6717b7c1eafcac2e789917c1
Rohitjaiswar123 <rohit.s.jaiswar@gmail.com>
2025-09-05 15:31:50 +0530

    serverless-fn-issue

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,5 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
-const path = require('path')
-module.exports = {
-  turbopack: {
-    root: path.join(__dirname, '..'),
-  },
-}
-
 
 export default nextConfig;
